dce76977954cca7b736c7417a642de99cc546ba7
ritchie-gr8 <rit.yotsathon@gmail.com>
2024-04-08 20:29:26 +0700

    feat: add answers

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,19 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (const v of array) {
+    operation(v)
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
+const raiseSalary = (oldSalary) => {
+  newEmployeeSalaries.push(oldSalary + 5000)
+}
+
+forEach(employeeSalaries, raiseSalary)
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
